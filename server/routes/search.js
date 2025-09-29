1c4ebbb24a45598eb19c5383e903b3f4ff360628
isdelr <rosvelt005@hotmail.com>
2025-09-29 17:05:41 -0400

    remove old js files that are not needed anymore

--- server/routes/search.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { searchProducts } from '../controllers/productsController.js';
-
-const router = Router();
-
-router.get('/', searchProducts);
-
-export default router;
